d7df513921651c3c053371d7d4768627d28cc99f
Ellie Joffe <ellijoffe1@gmail.com>
2023-05-16 18:21:46 +0200

    lecturer dashboard information capturing and interaction

--- mainRoutes.js
+++ mainRoutes.js
@@ -1,40 +1,48 @@
-const path = require('path');
-const express = require('express');
-const mainRouter = express.Router();
-const login = require('./s_login');
-const signup = require('./s_signup');
-const app = express();
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+const login = require('./s_login')
+const signup = require('./s_signup')
+const dashboard = require('./s_dash')
+const app = express()
 
-mainRouter.use('/', express.static(path.join(__dirname, 'public', 'resources')));
-app.use(express.json()); // This line is very important
+mainRouter.use('/', express.static(path.join(__dirname, 'public', 'resources')))
+app.use(express.json()) // This line is very important
 
 mainRouter.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'signup_login.html'));
-});
+  res.sendFile(path.join(__dirname, 'public', 'signup_login.html'))
+})
 
 mainRouter.get('/lecturer_dashboard', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'lecturer_dashboard.html'));
-});
+  res.sendFile(path.join(__dirname, 'public', 'lecturer_dashboard.html'))
+})
 
 mainRouter.get('/student_portal_page', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'student_portal_page.html'));
-});
+  res.sendFile(path.join(__dirname, 'public', 'student_portal_page.html'))
+})
 
-//Route to handle signup POST
+// Route to handle signup POST
 mainRouter.post('/signup', async function (req, res) {
-  res.type('application/json');
-  const result = await signup.addUser(req.body.name, req.body.email, req.body.password, req.body.role);
-  res.send(result);
-});
+  res.type('application/json')
+  const result = await signup.addUser(req.body.name, req.body.email, req.body.password, req.body.role)
+  res.send(result)
+})
 
-
-//Route to handle login POST
+// Route to handle login POST
 mainRouter.post('/login', async function (req, res) {
-  res.type('application/json');
-  //login is the MODULE we defined in login.js, and checkCredentials public by exporting it from login.js
-  const result = await login.checkCredentials(req.body.email, req.body.password);
-  res.send(result);
+  res.type('application/json')
+  // login is the MODULE we defined in login.js, and checkCredentials public by exporting it from login.js
+  const result = await login.checkCredentials(req.body.email, req.body.password)
+  res.send(result)
+})
 
-});
+// Route to handle dashboard POST
+mainRouter.post('/dashboard', async function (req, res) {
+  res.type('application/json')
+  const { dayOfMonth, startTime, recurringOptionBinary, endDate, endTime, maxConsultsDay, maxConsultsStudents, description } = req.body
+  const result = await dashboard.checkInfo(dayOfMonth, startTime, recurringOptionBinary, endDate, endTime, maxConsultsDay, maxConsultsStudents, description)
 
-module.exports = mainRouter;
+  res.send(result)
+})
+
+module.exports = mainRouter
