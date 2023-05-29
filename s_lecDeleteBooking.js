bf908cb74cdc73caf2194ed85efb52a04d5ff788
Ellie Joffe <ellijoffe1@gmail.com>
2023-05-29 08:28:56 +0200

    update student view and fix date issue

--- s_lecDeleteBooking.js
+++ s_lecDeleteBooking.js
@@ -3,7 +3,7 @@ const conn = require('./db_connection')
 async function lecDeleteBooking(bookingID) {
     try {
         const result = await conn.promise().query('DELETE FROM event_booking WHERE Id = ?', [bookingID])
-        console.log(result)
+        // console.log(result)
         return { status: 'Completed' }
     } catch (error) {
         console.error('Error retrieving events:', error)
